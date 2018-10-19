80a857a29518f6cd13d00fc059287b1bcc8f2eea
khardrix <43048160+khardrix@users.noreply.github.com>
2018-10-18 21:12:57 -0400

    Add files via upload

--- Email.java
+++ Email.java
@@ -25,69 +25,66 @@ package emailApp;
  *****************************************************************************************
  ****************************************************************************************/
 
-
-/*
-Project 1
-Email Application
-Scenario: You are an IT Support Administrator Specialist and are
-charged with the task of creating email accounts for new hires.
-Your application should do the following:
- Generate an email with the following syntax: firstname.lastname@department.company.com
- Determine the department (sales, development, accounting), if none leave blank
- Generate a random String for a password
- Have set methods to change the password, set the mailbox capacity, and define an alternate
-email address
- Have get methods to display the name, email, and mailbox capacity
- */
-
 // IMPORTS of needed tools and plug-ins.
 import java.util.Random;
-import java.util.Scanner;
+
 
 public class Email {
 
     private String firstName;
     private String lastName;
-    private String department;
-    private String[] password;
+    private String department;    // Sales, Development and Accounting.
+    private String password;
     private int mailboxCapacity;
     private String email;
     private String newPassword;
     private String emailAternate;
-    private Random dice = new Random();
-    private Scanner input = new Scanner(System.in);
-    private String[] letterLow = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};         // 26
-    private String[] letterUpper = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};         // 26
-    private String[] numberArr = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};                 // 10
-    private String[] symbols = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+"};    // 13
-    private int textType;
-    private int randomNumber;
-    private int passwordMaxLength;
-    private int passwordLength;
-    private String currPassChar;
+
+
+    public Email(String firstName, String lastName, String department){
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        password = generatePassword();
+        email = firstName + "." + lastName + "@" + department + ".company.com";
+    }
 
 
     public Email(String firstName, String lastName){
+
         this.firstName = firstName;
         this.lastName = lastName;
+        password = generatePassword();
+        email = firstName + "." + lastName + "@company.com";
     }
 
 
     public String generatePassword(){
 
-        passwordMaxLength = 4 + dice.nextInt(21);
-        for(int i = 0; i <= passwordMaxLength; i++){
-            textType = dice.nextInt(4);
-            password[i] = getCurrPassChar(textType);
+        Random dice = new Random();
+        int passwordMaxLength = 4 + dice.nextInt(21);
+        StringBuilder randomPassword = new StringBuilder(passwordMaxLength);
+
+        for(int i = 0; i < passwordMaxLength; i++){
+            int textType = dice.nextInt(4);
+            randomPassword.append(getCurrPassChar(textType));
         }
-        return password.toString();
+        return randomPassword.toString();
     }
 
 
     public String getCurrPassChar(int type){
 
+        Random dice = new Random();
+        int randomNumber;
+        String currPassChar = "";
+        String[] letterLow = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};         // 26
+        String[] letterUpper = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};         // 26
+        String[] numberArr = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};                 // 10
+        String[] symbols = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+"};    // 13
 
         if(type == 0){
             randomNumber = dice.nextInt(26);
@@ -105,4 +102,39 @@ public class Email {
         return currPassChar;
     }
 
+
+    public void setPassword(String password){
+
+        this.password = password;
+    }
+
+
+    public void setMailboxCapacity(int capacity){
+
+        this.mailboxCapacity = capacity;
+    }
+
+
+    public void setEmailAternate(String email){
+
+        this.emailAternate = email;
+    }
+
+
+    public String getName(){
+
+        return firstName + " " + lastName;
+    }
+
+
+    public String getEmail(){
+
+        return email;
+    }
+
+
+    public int getMailboxCapacity(){
+
+        return mailboxCapacity;
+    }
 }
